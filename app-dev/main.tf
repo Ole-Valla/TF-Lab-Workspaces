4e653976a48bfe66a71ffbac00f482a87181286e
Ole Jørgen Valla Dønnem <ole.jorgen.valla.donnem@nhn.no>
2021-08-25 14:32:44 +0200

    fixing features

--- app-dev/main.tf
+++ app-dev/main.tf
@@ -1,12 +1,20 @@
 #====================================================================
 #=================== RESOURCE GROUP =================================
 #====================================================================
+provider "azurerm" {
+
+  features {
+
+  }
+
+}
+
 resource "azurerm_resource_group" "module" {
   name     = "${var.name}-rg"
   location = "${var.location}"
 
   tags = {
     environment = "dev"
-    owner       = "Ole"
+    owner       = "Oledo "
   }
 }
