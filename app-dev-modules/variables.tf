3c90b479cf7953b783d0b0c4446b7258e938948a
Ole Jørgen Valla Dønnem <ole.jorgen.valla.donnem@nhn.no>
2021-08-25 15:11:39 +0200

    fixing variables in app-dev-modules

--- app-dev-modules/variables.tf
+++ app-dev-modules/variables.tf
@@ -1,18 +1,22 @@
 variable "name" {
   description = "The name of the resource being created."
-  # default = "dev-server-1"
+  default = "dev-server-1"
 }
 variable "location" {
   description = "The location of the network thats being created."
+  default = "norwayeast"
 }
 variable "username" {
   description = ""
+  default = "oledo"
 }
 variable "password" {
   description = ""
+  default = "Password123"
 }
 variable "vm_size" {
   description = "Specifies the size of the Virtual Machine."
+  default = "Standard_B1s"
 }
 
 variable "vnet_address_spacing" {
